4bd393de32fd8dab9b1096565a0b3b72b9688a99
Austo0 <austenfogarty@gmail.com>
2016-10-12 13:14:22 +1300

    1

--- buttonint.c
+++ buttonint.c
@@ -13,13 +13,13 @@
 #include "lcd.h"
 #include <util/delay.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
  // Initialise the timer and pin change interrupts
  void Initialise_Interrupts()
  {
-
 	        /* Initialise timer2 interrupts */
 	        OCR2A = 128;							// Initial value to count to
 	        TCCR2A |= (1 << COM2A1);				// Set non-inverting mode
@@ -36,45 +36,38 @@ int main(void)
 
 	/* Set up Port B; output on Bit 3  */
 	DDRB |= (1 << PB3) ;
-	//DDRD &= ~(1 << PD2);
 
 	/* No pull-up resistor on inputs */
 	PORTB = 0x00;
-	PORTB &= ~(1 << PD2);
 
 	lcd_init('c');		// Initialise the LCD
-	char str[20];
-	int ADC1;
+	char str[20];		// Display
+	char voltage[20];	// Voltage
+	int ADC1;			// Measured ADC
 
 	while (1)
 	{
-		//OCR2A = (1 << 7);	// 50% duty cycle
+		OCR2A = (1 << 7);	// 50% duty cycle
 
 		 		for(int i = 7; i >= 0; i--)
 		 		{
 		 			OCR2A |= (1 << i);	// Set the bit to check
 		 			_delay_ms(50);
 
-//		 			if(PIND & (1 << PD2))
-//		 			{
-//		 				OCR2A |= (1 << i);
-//
-//		 			}
-//		 			else
-		 			if(!(PIND & (1 << PD2)))
+
+		 			if(!(PINB & (1 << PB0)))
 		 			{
 		 				OCR2A &= ~(1 << i);
 		 			}
-//		 			//_delay_ms(100);
 		 		}
-//		 		ADC1 = OCR2A;
-//
-		 		snprintf(str,20,"%i",ADC1);
-//		 		lcd_display(str,0);		// Display the time
-//		 		_delay_ms(500);			// Short delay
-//		 		lcd_display("clear",0);
-//		 		_delay_ms(500);
-//		 		lcd_clear();			//Clear the LCD
+		 		ADC1 = OCR2A;
+		 		dtostrf(((double)5/255)*ADC1,4,2,voltage);
+
+		 		snprintf(str,20,"V = %s",voltage);
+
+		 		lcd_clear();			//Clear the LCD
+		 		lcd_display(str,0);		// Display the time
+
 
 	}
 	return 0;
