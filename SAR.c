b467086231056fbd8136bcf276eadb4720e03645
Austen <austenfogarty@gmail.com>
2016-10-13 21:29:33 +1300

    compare adc

--- SAR.c
+++ SAR.c
@@ -28,6 +28,72 @@
 	        sei();
  }
 
+
+ /*
+  * adc_init()
+  * Power up the ADC hardware and select the ADC input pin.
+  * pin must be set to a value listed in Table 23-4 of the ATmega manual
+  */
+ void adc_init(int pin)
+ {
+ 	pin &= 0xf;
+
+ 	/* Make sure the ADC circuit is powered up */
+ 	PRR &= ~(1<<PRADC);
+
+ 	/* Turn off GPIO on the specified analogue input pin */
+ 	if (pin <= 7) {
+ 		/* Only the first eight inputs correspond to GPIO on Port C */
+ 		DIDR0 = 1<<pin;
+ 	}
+
+ 	/* Set up ADC control registers ready for conversions */
+         /* Enable ADC and set the lowest bit clock rate. */
+ 	ADCSRA = (1<<ADEN) | 0x07;
+
+ 	/* Unused so lets just set ADCSRB to 0 */
+ 	ADCSRB = 0;
+
+ 	/* Select ADC input source. */
+ 	if (pin == 8) {
+ 		/* Voltage ref is 1.1V for CPU temperature sensor */
+ 		ADMUX = (1<<REFS1) | (1<<REFS0) | pin;
+ 	}else{
+ 		/* Voltage ref is AVcc (5V) for all other inputs */
+ 		ADMUX = (1<<REFS0) | pin;
+ 	}
+ }
+
+ /*
+  * adc_read()
+  *
+  * Perform an analogue to digital conversion and return
+  * the converted value.
+  * Input source is specified by calling adc_init() first.
+  */
+
+ int adc_read(void)
+ {
+ 	int val;
+
+ 	ADCSRA |= (1<<ADSC);	//Trigger the ADC
+
+ 	while (ADCSRA & (1<<ADSC) )	//Wait for the ADC to finish
+ 	{
+
+ 	}
+
+ 	val = ADC;	// Store the 10 bit ADC value
+
+ 	return val;	//Return the 10 bit value
+ }
+
+
+
+
+
+
+
 // Slow SAR conversion
 double SAR_Conversion()
  {
@@ -113,18 +179,21 @@ int main(void)
 
 	/* No pull-up resistor on inputs */
 	PORTB = 0x00;
-
+	adc_init(0);	//Initalise the ADC
 	lcd_init('c');		// Initialise the LCD
 	char str[34];		// Display
 	char Svoltage[20];	// standard deviation voltage
 	char Mvoltage[20];	// mean voltage
 	char S[20];			// standard deviation ADC
 	char M[20];			// mean ADC
-
+	char adc_numval[20];		//ADC value
+	char vadc_numval[20];		//ADC value
 	int N = 500;
 	int nsamples[N];	// voltage
+
 	while (1)
 	{
+
 		// For the specified number of samples
 		for(int n = 0; n < N; n++)
 		{
@@ -137,9 +206,15 @@ int main(void)
 		dtostrf(Mean(nsamples,N),5,3,M);							// Mean ADC value
 		dtostrf(((double)5.04/255)*Mean(nsamples,N),5,4,Mvoltage);	// Mean voltage
 		dtostrf(Standard_Deviation(nsamples,N),5,4,S);				// Standard Deviation ADC value
+		for(int n = 0; n < N; n++)
+			{
+			nsamples[n] = adc_read();
+			}
+		dtostrf(((double)5.04/1023)*Mean(nsamples,N),5,4,vadc_numval);	// Mean voltage
+		dtostrf(Mean(nsamples,N),5,4,adc_numval);	// Mean voltage
 
-
-		 snprintf(str,34,"M: %sV\nS:%sV %s",Mvoltage,Svoltage,S);	// Convert to string
+		 //snprintf(str,34,"M:%sV%s\nS:%sV",Mvoltage,adc_numval,Svoltage);	// Convert to string
+		 snprintf(str,34,"M:%sV%s\nS%s %s",Mvoltage,vadc_numval,M,adc_numval);	// Convert to string
 		 _delay_ms(200);
 		 lcd_clear();												// Clear the LCD
 		 lcd_display(str,0);										// Display the voltage on the LCD
